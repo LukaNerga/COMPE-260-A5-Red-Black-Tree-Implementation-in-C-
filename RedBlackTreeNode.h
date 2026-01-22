b98e8ba878851c15dec5e766314814174ff14e1c
Luka Nergadze <160408244+LukaNerga@users.noreply.github.com>
2026-01-22 20:05:27 +0400

    Refactor RedBlackTreeNode class definition

--- RedBlackTreeNode.h
+++ RedBlackTreeNode.h
@@ -1,33 +1,31 @@
-#ifndef REDBLACKTREE_H
-#define REDBLACKTREE_H
-#include "RedBlackTreeNode.h"
-class RedBlackTree {
+#ifndef REDBLACKTREENODE_H
+#define REDBLACKTREENODE_H
+
+enum ColorType { RED, BLACK };
+
+class RedBlackTreeNode {
+    friend class RedBlackTree;
+
 private:
-RedBlackTreeNode* root;
-RedBlackTreeNode* nil;
-void leftRotate(RedBlackTreeNode* x);
-void rightRotate(RedBlackTreeNode* y);
-void insertFixup(RedBlackTreeNode* z);
-void deleteFixup(RedBlackTreeNode* x);
-RedBlackTreeNode* minimum(RedBlackTreeNode* node) const;
-void transplant(RedBlackTreeNode* u, RedBlackTreeNode* v);
-// Utility Functions
-void preorderTraversal(RedBlackTreeNode* node) const;
-void inorderTraversal(RedBlackTreeNode* node) const;
-void postorderTraversal(RedBlackTreeNode* node) const;
-RedBlackTreeNode* searchNode(RedBlackTreeNode* node, int value) const;
+    int value;
+    ColorType color;
+    RedBlackTreeNode* parent;
+    RedBlackTreeNode* left;
+    RedBlackTreeNode* right;
+
 public:
-// Constructors and Destructor
-RedBlackTree();
-RedBlackTree(const RedBlackTree& other);
-~RedBlackTree();
-RedBlackTree& operator=(const RedBlackTree& other);
-// Public Member Functions
-void insert(int value);
-void deleteNode(int value);
-RedBlackTreeNode* search(int value) const;
-void preorder() const;
-void inorder() const;
-void postorder() const;
+    RedBlackTreeNode()
+        : value(0), color(RED), parent(nullptr), left(nullptr), right(nullptr) {}
+
+    RedBlackTreeNode(int inValue, ColorType inColor,
+                     RedBlackTreeNode* inLeft = nullptr,
+                     RedBlackTreeNode* inRight = nullptr,
+                     RedBlackTreeNode* inParent = nullptr)
+        : value(inValue), color(inColor),
+          parent(inParent), left(inLeft), right(inRight) {}
+
+    int getValue() const { return value; }
+    ColorType getColor() const { return color; }
 };
-#endif // REDBLACKTREE_H
+
+#endif // REDBLACKTREENODE_H
